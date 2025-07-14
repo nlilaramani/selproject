746fe4519cf5ba0bc63afa8c0743d153cda791c2
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2025-07-13 23:00:01 -0500

    Update SetupTest.java

--- src/test/java/com/sel/test/SetupTest.java
+++ src/test/java/com/sel/test/SetupTest.java
@@ -58,5 +58,18 @@ public class SetupTest {
         //driver.findElement(By.name("q")).sendKeys("Selenium jobs");
         driver.findElement(By.name("q")).sendKeys("Selenium jobs"+Keys.ENTER);
         //WebDriverManager.chromedriver().setup();
+        driver.get("https://sweetshop.vivrichards.co.uk/login");
+        driver.findElement(By.id("exampleInputEmail")).sendKeys("Narendra");
+        driver.findElement(By.id("exampleInputEmail")).clear();
+        driver.findElement(By.id("exampleInputEmail")).sendKeys("nrlilaramani@hotmail.com");
+       // driver.findElement(By.id("exampleInputEmail")).submit();
+        
+        FileInputStream fs=new FileInputStream(new File("c:\\data\\login.xlsx"));
+        Workbook wb=new XSSFWorkbook(fs);
+        String un=wb.getSheetAt(0).getRow(1).getCell(0).getStringCellValue();
+        driver.findElement(By.id("exampleInputEmail")).clear();
+        driver.findElement(By.id("exampleInputEmail")).sendKeys(un);
+        fs.close();
+
     }
 }
